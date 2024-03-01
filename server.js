68c6a091d5e652007360d6f892c6a9409bd84caf
nicholasstacy89 <116467600+nicholasstacy89@users.noreply.github.com>
2024-02-29 19:37:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const bcrypt = require('bcrypt');
-const helpers = require('./helpers');
+const helpers = require('./utils/helpers');
 const routes = require('./controllers');
-const sequelize = require('sequelize');
+const sequelize = require('./config/connection');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
