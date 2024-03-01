ace9c2978d81d2fdc1c4883f7013e5ad2505ac1e
Austinma10 <austinma2168@gmail.com>
2024-02-29 19:35:47 -0500

    create index.js and homeRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { Workouts, User } = require('../models');
+
+router.get('/login', (req,res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: {exclude: ['password']},
+            include: [{ model: Workouts }],
+        });
+
+        const user = userData.get({plain: true});
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (error) {
+        console.error(error);
+    }
+})
