991e755babf2d140f195cbc6d19caf005b0c2659
amaher <amaher112@su.edu>
2024-03-04 20:30:48 -0500

    "added basic code for personal stats"

--- public/js/personalStats.js
+++ public/js/personalStats.js
@@ -1,5 +1,24 @@
 const calculate = require('fitness-health-calculations');
 
-// basal metabolic 
-let myBmr = calculate.bmr('female', 22, 168, 65);
-let totalCaloricNeeds = calculate.caloricNeeds('male', 22, 195, 95, 'high', 'gain', 'agressive');
+const gender = document.querySelector('#gender-signup');
+const weight = document.querySelector('#weight-signup');
+const height = document.querySelector('#height-signup');
+const age = document.querySelector('#age-signup');
+const activity_level = document.querySelector('#activity-level');
+const weightGoal = document.querySelector('#user-goal');
+
+// basal metabolic rate
+let myBmr = calculate.bmr(gender, age, height, weight);
+console.log(myBmr);
+
+// total caloric needs
+let totalCaloricNeeds = calculate.caloricNeeds(gender, age, height, weight, activity_level, weightGoal, approach)
+
+// total daily energy expenditure
+let dailyEnergyExp = calculate.tdee(gender, age, height, weight, activity_level)
+console.log(dailyEnergyExp);
+
+// ideal body weight
+let idealBodyWeight = calculate.idealBodyWeight(height, gender)
+console.log(idealBodyWeight);
+
