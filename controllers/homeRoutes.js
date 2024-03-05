d9cba6a6d1394ed633e2c12810424af9f60f28a7
amaher <amaher112@su.edu>
2024-03-04 20:30:13 -0500

    "fixed some syntax errors"

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Workouts, User } = require('../models');
+const withAuth = require('../utils/auth')
 
 router.get('/login', (req,res) => {
     if (req.session.logged_in) {
@@ -27,3 +28,5 @@ router.get('/profile', withAuth, async (req, res) => {
         console.error(error);
     }
 })
+
+module.exports = router;
