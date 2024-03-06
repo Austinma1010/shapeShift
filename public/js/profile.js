cc5c1f42dbee4b6f84aa97f0bb3bfb9082688cca
amaher <amaher112@su.edu>
2024-03-05 21:35:22 -0500

    'created initial fetch call for personal stats'

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,27 +1,52 @@
 // User Dashboard Section
 
 // Personal stats section
-const calculate = require('fitness-health-calculations');
 const gender = document.querySelector('#gender-signup');
 const weight = document.querySelector('#weight-signup');
 const height = document.querySelector('#height-signup');
 const age = document.querySelector('#age-signup');
 const activity_level = document.querySelector('#activity-level');
 const weightGoal = document.querySelector('#user-goal');
+const bmrBtn = document.querySelector('#bmr');
+const tdeeBtn = document.querySelector('#tdee');
+const caloricNeedsBtn = document.querySelector('#caloric-needs');
+const idealWeightBtn = document.querySelector('#ideal-weight');
 
 // basal metabolic rate
-let myBmr = calculate.bmr(gender, age, height, weight);
+bmrBtn.addEventListener('click', function() {
+    // create fetch call
+    const data = { gender, weight, height, age, activity_level, weightGoal };
+
+    fetch('https://example.com/api/stats', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Network response was not ok.');
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 
 
-// total caloric needs
-let totalCaloricNeeds = calculate.caloricNeeds(gender, age, height, weight, activity_level, weightGoal, approach)
+//     fetch('http://localhost:3001/api/stats')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error('Error:', error));
 
-// total daily energy expenditure
-let dailyEnergyExp = calculate.tdee(gender, age, height, weight, activity_level)
+    // let myBmr = calculate.bmr(gender, age, height, weight);
+    // console.log(myBmr);
+})
 
-
-// ideal body weight
-let idealBodyWeight = calculate.idealBodyWeight(height, gender)
 
 
 
