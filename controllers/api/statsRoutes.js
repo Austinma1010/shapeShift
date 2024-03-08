68017ef1753bd938e64234b2a5a000fbeb5e0a4b
amaher <amaher112@su.edu>
2024-03-07 19:55:57 -0500

    finished stats routes

--- controllers/api/statsRoutes.js
+++ controllers/api/statsRoutes.js
@@ -16,7 +16,7 @@ router.get("/bmr/:id", async (req, res) => {
     } else {
       newGender = "female";
     }
-    // const stats = newStats.get({plain: true});
+
 
     const myBmr = calculate.bmr(newGender, age, height, weight);
     console.log(myBmr);
@@ -27,10 +27,10 @@ router.get("/bmr/:id", async (req, res) => {
 });
 
 // total caloric needs
-// let totalCaloricNeeds = calculate.caloricNeeds(gender, age, height, weight, activity_level, weightGoal)
+
 router.get("/caloricneeds/:id", async (req, res) => {
   try {
-    const newStats = await User.findByPk(req.params.id, {
+    const newCaloricNeedsStats = await User.findByPk(req.params.id, {
       attributes: [
         "gender",
         "age",
@@ -41,7 +41,7 @@ router.get("/caloricneeds/:id", async (req, res) => {
       ],
     });
     const { gender, age, height, weight, activity_level, goal } =
-      newStats;
+      newCaloricNeedsStats;
 
     let newGender;
     if (gender) {
@@ -49,7 +49,6 @@ router.get("/caloricneeds/:id", async (req, res) => {
     } else {
       newGender = "female";
     }
-    // const stats = newStats.get({plain: true});
 
     let actLevel;
     switch (activity_level) {
@@ -85,9 +84,68 @@ router.get("/caloricneeds/:id", async (req, res) => {
 });
 
 // total daily energy expenditure
-// let dailyEnergyExp = calculate.tdee(gender, age, height, weight, activity_level)
+router.get("/tdee/:id", async (req, res) => {
+  try {
+    const tdeeStats = await User.findByPk(req.params.id, {
+      attributes: ["gender", "age", "height", "weight", "activity_level"],
+    });
+    const { gender, age, height, weight, activity_level } = tdeeStats;
+
+    let newGender;
+    if (gender) {
+      newGender = "male";
+    } else {
+      newGender = "female";
+    }
+
+    let actLevel;
+    switch (activity_level) {
+      case 1:
+        actLevel = "sedentary";
+        break;
+      case 2:
+        actLevel = "light";
+        break;
+      case 3:
+        actLevel = "moderate";
+        break;
+      case 4:
+        actLevel = "high";
+        break;
+      case 5:
+        actLevel = "extreme";
+    }
+
+    const tdee = calculate.tdee(newGender, age, height, weight, actLevel);
+    console.log(tdee);
+    res.status(200).json(tdee);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // ideal body weight
-// let idealBodyWeight = calculate.idealBodyWeight(height, gender)
+
+router.get("/ibw/:id", async (req, res) => {
+  try {
+    const BodyWeight = await User.findByPk(req.params.id, {
+      attributes: ["height", "gender"],
+    });
+    const { height, gender } = BodyWeight;
+
+    let newGender;
+    if (gender) {
+      newGender = "male";
+    } else {
+      newGender = "female";
+    }
+
+    const myIdealBodyWeight = calculate.idealBodyWeight(height, newGender, "imperial");
+    console.log(myIdealBodyWeight);
+    res.status(200).json(myIdealBodyWeight);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
